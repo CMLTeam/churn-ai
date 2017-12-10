b62853be8fb123df7ed582c1fca6d03f2863cf03
sviridenich <sergey.sviridenko@gmail.com>
2017-12-10 11:54:56 +0200

    simplest model build where we achieved 85% ROCAUC

--- modeling/main.py
+++ modeling/main.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import cross_val_score
 
 from credentials import HOST, USERNAME, PASSWORD, DBNAME
 
@@ -8,31 +9,37 @@ import numpy as np
 
 if __name__=="__main__":
 
-    conn_str = "mysql+pymysql://{0}:{1}@{2}/{3}".format(USERNAME, PASSWORD , HOST, DBNAME)
+    conn_str = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format(USERNAME, PASSWORD , HOST, DBNAME)
 
     engine = create_engine(conn_str)
     metadata = MetaData()
     conn = engine.connect()
-    dataset = Table('dataset', metadata,
+
+    dataset = Table('dataset_cut', metadata,
                   Column('hash_number_A', Integer),
                   Column('dd', Date),
                   Column('horyzon', Integer),
-                  Column('target', Boolean),
-                  Column('recency_factor', Integer),
-                  Column('frequency_factor', Float),
-                  Column('monetary_factor', Float))
+                  Column('t', Integer),
+                  Column('recency', Integer),
+                  Column('frequency', Float),
+                  Column('monetary', Float))
 
     s = select([dataset])
     result = conn.execute(s)
 
-    # X = np.empty((0, 100))
-    # y = np.empty((0, 100))
     X = list()
     y = list()
+
+    rows = 0
     for row in result:
-        hash_number_A, dd, horyzon, target, recency_factor, frequency_factor, monetary_factor = row
-        X.append((horyzon, target, recency_factor, frequency_factor, monetary_factor))
-        y.append(target)
+        hash_number_A, dd, horyzon, t, recency, frequency, monetary = row
+        X.append((horyzon, recency, frequency, monetary))
+        y.append(t)
+        rows = rows + 1
 
     lgr = LogisticRegression()
     lgr.fit(X=X, y=y)
+    crossval_scores = cross_val_score(lgr, X, y, scoring='roc_auc')
+    # 85%
+    print('ROC-AUC test run scores: ', crossval_scores)
+    print(rows)
